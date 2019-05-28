edfab8ee73463a9ef6001e2616d15abc9f7bc349
Cherrison <1197750416sh@gmail.com>
2019-05-28 21:34:52 +0800

    播放器还差点

--- pages/home/home.js
+++ pages/home/home.js
@@ -35,22 +35,6 @@ Component({
     StatusBar: t.globalData.StatusBar,
     CustomBar: t.globalData.CustomBar,
     cardCur: 0,
-    stickyNote: {
-      friend_id: 55,
-      user_id: 12,
-      virtual_name: "只懂你心",
-      content: "我很好奇为什么我喜欢的男生（ 他不喜欢我， 而且他知道我喜欢他） 一直躲避我的目光， 很无语， 我又怎么才能让他知道我不喜欢他了呢?",
-      bg_id: 12,
-      bg_img: "https://www.52hertalk.cn/public/upload/friendBg/2018/12-11/62b2933ea6013dd694a5a7404670708c.jpg",
-      like_num: 125,
-      dislike_num: 3,
-      like_status:1
-    },
-    card:{
-      bg: "gradual-blue",
-      name: "只懂你心",
-      color: "我很好奇为什么我喜欢的男生（ 他不喜欢我， 而且他知道我喜欢他） 一直躲避我的目光， 很无语， 我又怎么才能让他知道我不喜欢他了呢?"
-    },
     bgAudioState: {
       starttime: "00: 00",
       endtime: "03: 21",
@@ -75,24 +59,15 @@ Component({
     listenIndex:0,
     listenList: [{
       id: 74,
-      coverImgUrl: "https://www.52hertalk.cn/public/upload/listen/2019/01-14/e179a432d232e209ef84a5d0dd437ac0.png",
-      title: "此时此刻",
-      author: "小海",
-      boutique: 1,
+      coverImgUrl: "https://www.cheery.pro/radio/goodgirl.png",
+      title: "余生那么长，别总自己扛",
+      author: "哎？是我", 
       classify: 1,
-      src: "http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46",
+      src: "https://www.cheery.pro/radio/goodgirl.mp3",
       rank: 0,
       hot: 28293
     }],
     articleList: [{
-      articleId: '1',
-      title: '这里有个戏精铲屎官，主子了解一下？',
-      image: 'https://image.weilanwl.com/img/4x3-3.jpg',
-      description: '这是一个伪铲屎官为了给自己的程序凑字数瞎几把乱写的一堆文字，了解一下就OK！ヾ(=･ω･=)o',
-      content: "",
-      views: "123",
-      author: "小海",
-      picUrl: "https://www.52hertalk.cn/public/upload/article/2019/05-05/bd0442b85c9130330a4972ffb3d60530.jpg",
       tags: [{
         short: "假装有猫",
         color: "red"
@@ -118,71 +93,14 @@ Component({
       var index = this.data.listenIndex
       if(app.data.src=="")
         app.setMusic(data[index].title, data[index].coverImgUrl, data[index].author, data[index].title, data[index].src)
+      this.play()
+      this.play()
       var that = this
       t.getUserInfo(function (usercb) {
         that.setData({
           userinfo: usercb,
           nickName: usercb.nickName,
           imageurl: usercb.avatarUrl
-        })//下面获取文章列表
-        wx.request({
-          url: 'https://www.clearn.site/wxapi/getArticle.php',
-          method:"POST",
-          header:{
-            "Content-Type": "application/x-www-form-urlencoded"
-          },
-          data:{
-            type:'title',
-            id:0,
-            num:5
-          },
-          success:function(res){
-            console.log(res)
-            if(!res.data)
-              return
-            var tmp = []
-            for(let i = 0;i < 5 && i<res.data.length;i++)
-              tmp.push(res.data[i])
-            that.setData({
-              articleList:tmp
-            })
-          }
-        })//下面获取歌曲列表
-        wx.request({
-          url: 'https://www.clearn.site/wxapi/getListenList.php',
-          method: "POST",
-          header: {
-            "Content-Type": "application/x-www-form-urlencoded"
-          },
-          data: {
-            type: 'get',
-            id: 0,
-            num: 15
-          },
-          success: function (res) {
-            console.log(res)
-            if (res.data == null) {
-              console.log("没有了...")
-              return
-            }
-            else if (res.data == "获取失败")
-              return
-            that.setData({
-              listenList: res.data,
-            })//处理跳转页面歌曲状态
-            if (app.data.src != "")
-              for (let i = 0; i < that.data.listenList.length; i++)
-                if (that.data.listenList[i].src == app.data.src) {
-                  console.log(i)
-                  that.setData({
-                    listenIndex: i,
-                    'bgAudioState.playState': 1
-                  })
-                  if (app.data.isPlay)
-                    that.play()
-                  return
-                }
-            }
         })
         console.log('用户名称: ', usercb)
         console.log('用户名称1: ', that.data.userinfo)
@@ -205,7 +123,6 @@ Component({
         }
     },
     onReady: function (options) {
-      console.log("")
       var app = getApp()
       var length = app.getDuration()
       var m = parseInt(length / 60)
@@ -282,23 +199,27 @@ Component({
       })
     },
     play: function (t) {
+      console.log(a)
       var app = getApp()
       if (this.data.bgAudioState.playState == 0) {
         console.log('播放')
         this.setData({
           bgAudioState: this.data.playing
         })
+        this.setData({
+          endTime: util.timeform(a.duration)
+        }) 
         app.data.inter = setInterval(this.setTime, 1000, this)
         app.playMusic()
-        this.setData({
-          maxTime:app.getDuration()
-        })
       } else {
         console.log('暂停')
         app.pauseMusic()
         this.setData({
           bgAudioState: this.data.pause
         })
+        this.setData({
+          endTime: util.timeform(a.duration)
+        }) 
         this.endTime(this)
         clearTimeout()
       }
@@ -316,6 +237,9 @@ Component({
       app.setMusic(data[index].title, data[index].coverImgUrl, data[index].author, data[index].title, data[index].src)
       if(!this.data.bgAudioState.playState)
         this.play()
+      this.setData({
+        endTime: util.timeform(a.duration)
+      }) 
     },
 
     nextSong:function(e){
@@ -330,6 +254,9 @@ Component({
       app.setMusic(data[index].title, data[index].coverImgUrl, data[index].author, data[index].title, data[index].src)
       if (!this.data.bgAudioState.playState)
         this.play()
+      this.setData({
+        endTime: util.timeform(a.duration)
+      }) 
     },
     endTime(that) {
       var app = getApp()
@@ -338,6 +265,33 @@ Component({
     // 文章部分
     getData: function () {
       var that = this
+      //下面获取歌曲列表
+      wx.request({
+        url: 'https://www.clearn.site/wxapi/getListenList.php',
+        method: "POST",
+        header: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        data: {
+          type: 'get',
+          id: 0,
+          num: 5
+        },
+        success: function (res) {
+          console.log(res)
+          if (res.data == null) {
+            console.log("没有了...")
+            return
+          }
+          else if (res.data == "获取失败") {
+            console.log("error")
+            return
+          }
+          that.setData({
+            listenList: res.data,
+          })
+        }
+      })
       wx.request({
         url: 'https://www.clearn.site/wxapi/getArticle.php',
         method: "POST",
